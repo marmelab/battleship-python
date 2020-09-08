20f56f0a841891ce2925f8a08e03ac4dda0bf903
Benjamin Payoux <benjamin.payoux@protonmail.ch>
2020-09-08 10:11:10 +0200

    Add test for the board configuration

--- src/Board_test.py
+++ src/Board_test.py
@@ -1,6 +1,9 @@
 import unittest
+import os
 
-from Board import initBoard
+from Board import initBoard, getBoardFromConfig
+
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), '../config/player1')
 
 class TestMainMethods(unittest.TestCase):
 
@@ -14,3 +17,25 @@ class TestMainMethods(unittest.TestCase):
             [0, 0, 0, 0, 0]
         ]
         self.assertEqual(initBoard(5), board)
+
+    def test_board_config(self):
+        
+        # os.path.relpath('..\\subfldr1\\testfile.txt', os.path.dirname(__file__))
+        # f = open("./config/player1", "r")
+        with open("./config/player1", 'r') as f:
+            config = f.read().splitlines()
+
+        board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 1, 1, 1, 0, 0, 0]
+        ]
+
+        self.assertEqual(getBoardFromConfig(config), board)
