5001b825dea1efa52bf4adeec76890b5787f9650
Benjamin Payoux <benjamin.payoux@protonmail.ch>
2020-09-09 16:38:48 +0200

    Add test for shipFoundAt method

--- src/Board_test.py
+++ src/Board_test.py
@@ -1,7 +1,8 @@
 import unittest
 import os
 
-from Board import initBoard, getBoardFromConfig
+from Board import initBoard, getBoardFromConfig, shipFoundAt
+from Game import initGame
 from constants import PLAYER_1, PLAYER_2, FLEET_LIFE
 import ui
 
@@ -38,3 +39,14 @@ class TestMainMethods(unittest.TestCase):
         ]
 
         self.assertEqual(getBoardFromConfig(config), board)
+
+    def test_shipFoundAt(self):
+        # given a configuration
+        with open("./config/player1", 'r') as f:
+            config = f.read().splitlines()
+
+        boards, currentPlayer = initGame(config, "", 10)
+
+        # when checking a coordinate
+        # it should find a ship
+        self.assertTrue(shipFoundAt("B8", boards[PLAYER_1]["primary"]))
